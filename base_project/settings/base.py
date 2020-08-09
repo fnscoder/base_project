1f27c71afaeca769a2fe0591dfa5fda06c99e254
Felipe N Souza <fnscoder@gmail.com>
2020-08-09 18:47:31 -0300

    Updated .env file

--- base_project/settings/base.py
+++ base_project/settings/base.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(BASE_DIR)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
